4203468dabeeaa4d6fc2b9843ad5e9e46aec59ef
human-myriapoda <myriam-prasow@hotmail.com>
2024-07-25 14:42:17 +0100

    create IslandTimeConnections

--- run_workflow.py
+++
@@ -1,5 +0,0 @@
-from IslandTime import Workflow, retrieve_island_info
- 
-for island in ['Gan (Gaafu Dhaalu)']: 
-  
-    ii = Workflow(island, 'Maldives', run_all=False, execute_analysis=True, execute_segmentation=False, execute_preprocess=False, update_maps=False, overwrite_analysis=True).main()
